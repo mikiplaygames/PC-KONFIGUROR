0cb187ec0a24562b30c8ca5b8968c1e3478e1414
Doc <82221725+ThatDoc@users.noreply.github.com>
2023-01-27 17:20:28 +0100

    Scene changes

--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Home : MonoBehaviour
 {
     public void EnterScene(string scene)
     {
-        SceneManager
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
     }
 }
